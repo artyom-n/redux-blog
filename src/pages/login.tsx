6a7537323195566a8fd545a57fb7ba9e77c5ba71
adirins <andris.dirins@gmail.com>
2020-12-29 13:02:50 +0200

    Added Login Content

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,67 @@
-import React from 'react';
+import React from 'react'
+import { Form, Field } from 'react-final-form'
+
+
 
 const Login = () => {
-    return (
-        <section>
-            <div className="container">
-                <div className="row center-xs">
-                    <div className="col-xs-3">
-                        <h3>Login page</h3>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <Form
+      onSubmit={value => {
+        console.log(value);
+      }}
+    >
+      {({ handleSubmit, form, submitting, pristine }) => (
+        <form onSubmit={handleSubmit}>
+
+          <div>
+            <label>First Name</label>
+            <Field name="firstName">
+              {({ input }) => (
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  {...input} />
+              )}
+            </Field>
+          </div>
+          <div>
+            <label>Last Name</label>
+            <Field name="secondName">
+              {({ input }) => (
+                <input
+                  type="text"
+                  placeholder="Second Name"
+                  {...input} />
+              )}
+            </Field>
+          </div>
+          <div>
+            <label>Password</label>
+            <Field name="password">
+              {({ input }) => (
+                <input
+                  type="password"
+                  placeholder="Password"
+                  {...input} />
+              )}
+            </Field>
+          </div>
+          <div className="buttons">
+            <button type="submit" disabled={submitting || pristine}>
+              Submit
+              </button>
+            <button
+              type="button"
+              onClick={form.reset}
+              disabled={submitting || pristine}
+            >
+              Reset
+              </button>
+          </div>
+        </form>
+      )}
+    </Form >
+  );
+};
 
 export default Login;
