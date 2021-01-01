bc6838b8c912794bdf8f4cce011bec56aff6bd8f
WorkPrompt <workprompt@gmail.com>
2021-01-01 12:32:06 +0200

    Welcome text changed

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../store';
 import { logoutUser } from '../store/login/actions';
 
@@ -14,21 +14,21 @@ const Navigation = () => {
     <header className="header">
       <div className="container">
         <div className="row center-xs middle-xs">
-          <div className="col-xs-3">
+          <div className="col-xs-4">
             <div className="logo">
               <img src="https://www.krenerbookkeeping.com/wp-content/uploads/2017/05/blogger-logo-icon-png-22.png" alt="logo" width="50" />
             </div>
           </div>
-          <div className="col-xs-6">            
-              <Link className="Link" to="/">Home</Link>
-              <Link className="Link" to="/about">About</Link>
-              <Link className="Link" to="/login">Login</Link>            
+          <div className="col-xs-4">
+            <Link className="Link" to="/">Home</Link>
+            <Link className="Link" to="/about">About</Link>
+            <Link className="Link" to="/login">Login</Link>
           </div>
-          <div className="col-xs-3">
-            {userState.length === 0 ? "" : `You are logged in as ${userState[0].type}`}
-            {userState.length === 1 && 
+          <div className="col-xs-4">
+            {userState.length === 0 ? "" : `${userState[0].firstName}, you are logged in as ${userState[0].type}`}
+            {userState.length === 1 &&
               <button onClick={() => dispatch(logoutUser())} className="add-comment-btn cursor-pointer">
-                Log out
+                Sign out
               </button>}
           </div>
         </div>
